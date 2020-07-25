2bc5ce7ca63a32faea87dd1e13136c8aec4de67b
Scott Menzer <smenzer@gmail.com>
2020-07-25 16:33:34 +0200

    format output with colors

--- install.sh
+++ install.sh
@@ -1,36 +1,64 @@
 #!/bin/bash
 # usage: bash <(curl -SsL https://raw.githubusercontent.com/smenzer/terminal-setup/master/install.sh)
 
+# check if tput exists
+if [ ! command -v tput &> /dev/null ]; then
+    # tput could not be found
+    BOLD=""
+    RESET=""
+    UL=""
+    RUL=""
+    FG_WHITE=""
+    FG_SKYBLUE=""
+    FG_ORANGE=""
+    BG_AQUA=""
+    FG_BLACK=""
+    FG_ORANGE=""
+else
+    BOLD=$(tput bold)
+    RESET=$(tput sgr0)
+    FG_WHITE=$(tput setf 7)
+    FG_SKYBLUE=$(tput setaf 122)
+    FG_ORANGE=$(tput setaf 208)
+    BG_AQUA=$(tput setab 45)
+    FG_BLACK=$(tput setaf 16)
+    FG_ORANGE=$(tput setaf 214)
+    UL=$(tput smul)
+    RUL=$(tput rmul)
+fi
+
 repo_default="git@github.com:smenzer/terminal.git"
 target_default="${HOME}/src/github.com/smenzer"
 
 # get repo
-read -r -p "Enter the repo to pull from (hit Enter to use the default \"${repo_default}\"): " repo
+printf "${FG_SKYBLUE}\nEnter the repo location (hit Enter to use the default ${UL}${repo_default}${RUL}):${RESET}\n"
+read -r -p "> " repo
 if [ ! "${repo}" ]; then
     repo=${repo_default}
 fi
 
 # get machine name
-read -r -p "Enter the machine name (hit Enter to use the default \"$(hostname)\"): " machine
+printf "${FG_SKYBLUE}\nEnter the machine name (hit Enter to use the default ${UL}$(hostname)${RUL}):${RESET}\n"
+read -r -p "> " machine
 if [ ! "${machine}" ]; then
 	machine=$(hostname)
 fi
 
 # get target directory
-read -r -p "Enter the parent directory to place the \"terminal\" repository in (hit Enter to use the default \"${target_default}\"): " target
+printf "${FG_SKYBLUE}\nEnter the parent directory to place the \"terminal\" repository in (hit Enter to use the default ${UL}${target_default}${RUL}):${RESET}\n"
+read -r -p "> " target
 if [ ! "${target}" ]; then
 	target=${target_default}
 fi
 if [ ! -d "${target}" ]; then
 	mkdir -p ${target}
 fi
-# we don't want to create the terminal directory because git clone will do that for us
 target="${target}/terminal"
 
 # clone the main repo
-echo "--[ Cloning repo from ${repo} into ${target}"
+printf "${FG_ORANGE}${BOLD}\n--[ Cloning repo from ${UL}${repo}${RUL} into ${UL}${target}${RUL}${RESET}\n"
 git clone ${repo} ${target}
 
-# run the setup script from the main repo
+# # run the setup script from the main repo
 cd ${target}
 bash ./install.sh -m ${machine} -t ${target}
--- uninstall.sh
+++ uninstall.sh
@@ -3,8 +3,35 @@
 
 target_default="${HOME}/src/github.com/smenzer/terminal"
 
+# check if tput exists
+if [ ! command -v tput &> /dev/null ]; then
+    # tput could not be found
+    BOLD=""
+    RESET=""
+    UL=""
+    RUL=""
+    FG_WHITE=""
+    FG_SKYBLUE=""
+    FG_ORANGE=""
+    BG_AQUA=""
+    FG_BLACK=""
+    FG_ORANGE=""
+else
+    BOLD=$(tput bold)
+    RESET=$(tput sgr0)
+    FG_WHITE=$(tput setf 7)
+    FG_SKYBLUE=$(tput setaf 122)
+    FG_ORANGE=$(tput setaf 208)
+    BG_AQUA=$(tput setab 45)
+    FG_BLACK=$(tput setaf 16)
+    FG_ORANGE=$(tput setaf 214)
+    UL=$(tput smul)
+    RUL=$(tput rmul)
+fi
+
 # get target directory
-read -r -p "Enter the location of the \"terminal\" directory (hit Enter to use the default \"${target_default}\"): " target
+printf "${FG_SKYBLUE}\nEnter the location of the \"terminal\" directory (hit Enter to use the default ${UL}${target_default}${RUL}):${RESET}\n"
+read -r -p "> " target
 if [ ! "${target}" ]; then
 	target=${target_default}
 fi
