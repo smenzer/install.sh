258dda1097675eace020a34335301e44d4556cd0
Scott Menzer <smenzer@gmail.com>
2023-12-09 22:15:55 -0500

    try to fix homebrew

--- install.sh
+++ install.sh
@@ -625,10 +625,8 @@ if is_mac; then
     ## Install and update homebrew
     print_action "Installing Homebrew..."
     if ! is_command brew; then
-        run 'curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh -o install.sh'
-        run 'yes "" | /bin/bash -c install.sh'
+        run '/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)'
         run 'brew doctor'
-        run 'rm -f install.sh'
     else
         print_skipped
     fi
